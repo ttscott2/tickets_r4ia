e39269b2b3702ebacc26093df291d7e2ebe7c1b5
ttscott2 <timothytscott3@gmail.com>
2016-07-23 09:47:09 -0500

    add authors to tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720173604) do
+ActiveRecord::Schema.define(version: 20160723143903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160720173604) do
     t.integer  "project_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "author_id"
+    t.index ["author_id"], name: "index_tickets_on_author_id", using: :btree
     t.index ["project_id"], name: "index_tickets_on_project_id", using: :btree
   end
 
@@ -49,4 +51,5 @@ ActiveRecord::Schema.define(version: 20160720173604) do
   end
 
   add_foreign_key "tickets", "projects"
+  add_foreign_key "tickets", "users", column: "author_id"
 end
